9cebef468c901d2f6d7df78e831911dd32713d4a
GowdhamSubramaniyan <gowdham.uk@icloud.com>
2026-09-17 14:13:16 +0100

    Create script.js

---
+++ src/day 109/script.js
@@ -0,0 +1,48 @@
+const status = document.getElementById("status");
+const userList = document.getElementById("userList");
+
+async function loadUsers() {
+
+    status.textContent = "Loading...";
+
+    try {
+
+        const response = await fetch(
+            "https://jsonplaceholder.typicode.com/users"
+        );
+
+        if (!response.ok) {
+            throw new Error("Failed to load users.");
+        }
+
+        const users = await response.json();
+
+        userList.innerHTML = "";
+
+        if (users.length === 0) {
+
+            status.textContent = "No users found.";
+
+            return;
+        }
+
+        status.textContent = "Users loaded!";
+
+        users.forEach((user) => {
+
+            const li = document.createElement("li");
+
+            li.textContent = user.name;
+
+            userList.appendChild(li);
+        });
+
+    } catch (error) {
+
+        status.textContent = "❌ Something went wrong.";
+
+        console.log(error);
+    }
+}
+
+loadUsers();
